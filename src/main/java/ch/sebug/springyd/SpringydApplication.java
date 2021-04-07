f42fffd58f642f611a96f7b14a62f4029a8630d1
sebug <sebastian.gfeller@gmail.com>
2021-04-07 10:14:26 +0200

    Adding initial project

---
+++ src/main/java/ch/sebug/springyd/SpringydApplication.java
@@ -0,0 +1,13 @@
+package ch.sebug.springyd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringydApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringydApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/sebug/springyd/SpringydApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.sebug.springyd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringydApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
