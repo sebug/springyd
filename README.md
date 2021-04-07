b6b8c1b2f3f8eea2eb880b0aed3914bbe45a026c
sebug <sebastian.gfeller@gmail.com>
2021-04-07 12:15:04 +0200

    Temporarily allowing in-memory user to see the page

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # SpringyD
 Small sample of using OpenID connect in Spring connecting to my Azure AD.
 
+For it to work we'll need an app in Azure AD.
+
+    az ad app create --display-name "SpringyD"
+
+Jot down the app id. We want to store this not in source control, so let's use an environment variable:
+
+    export SPRINGYD_APP_ID={the_guid_of_your_app}
+
+And then we'll reference it in application.properties
+
+    
--- src/main/java/ch/sebug/springyd/AuthenticationInformationController.java
+++ src/main/java/ch/sebug/springyd/AuthenticationInformationController.java
@@ -2,11 +2,16 @@ package ch.sebug.springyd;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
 
 @RestController
 public class AuthenticationInformationController {
+    @Value("${springyd.app_id}")
+    private String appID;
+
     @GetMapping("/authInfo")
     public AuthenticationInformation authInfo() {
+        System.out.println("Some information from the application properties: " + this.appID);
         return new AuthenticationInformation("anonymous", "Anonymous User");
     }
 }
